0c6a5cfbd95fb6344f7870682e46daaf88f89b86
Silvio Carrera <carrera.silvio@gmail.com>
2021-01-23 11:14:33 -0300

    Backing up code

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace WeaponMaker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Weapon _weapon;
+        public Weapon Weapon
+        {
+            get => _weapon;
+            set => _weapon = value;
+        }
+
+        public MainWindow()
+        {
+            _weapon = new Weapon();
+            InitializeComponent();
+        }
+
+        private void HandleExportJsonClicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ Weapon.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace WeaponMaker
+{
+    public class Weapon
+    {
+        private string _weaponName = "Pistol";
+
+        public string WeaponName
+        {
+            get
+            {
+                return _weaponName;
+            }
+            set
+            {
+                _weaponName = value;
+                RaisePropertyChanged(nameof(WeaponName));
+            }
+        }
+
+        private float _rateOfFire;
+
+        public string RateOfFire
+        {
+            get
+            {
+                return _rateOfFire.ToString();
+            }
+            set
+            {
+                var isNumeric = float.TryParse(value, out float n);
+                _rateOfFire = isNumeric ? n : 0;
+                RaisePropertyChanged(nameof(RateOfFire));
+            }
+        }
+
+        private int _damage;
+
+        public string Damage
+        {
+            get
+            {
+                return _damage.ToString();
+            }
+            set
+            {
+                var isNumeric = int.TryParse(value, out int n);
+                _damage = isNumeric ? n : 0;
+                RaisePropertyChanged(nameof(Damage));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
