d244f4cc34d3cf869ed40f9db9d00f2985e75c04
Silvio Carrera <carrera.silvio@gmail.com>
2021-03-21 11:20:36 -0300

    Showing the item index value with the name

---
+++ Source/IndexConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using WeaponMaker;
+
+namespace WeaponMaker
+{
+    public class IndexConverter : IValueConverter
+    {
+        public object Convert(object value, Type TargetType, object parameter, CultureInfo culture)
+        {
+            var item = (ListBoxItem)value;
+            var listView = ItemsControl.ItemsControlFromItemContainer(item) as ListBox;
+            int index = listView.ItemContainerGenerator.IndexFromContainer(item);
+            return index.ToString("000");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+}
